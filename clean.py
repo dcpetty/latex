b151f8447070e0e0b5e13f668bfa7877ebfc13af
dcpetty <1700736+dcpetty@users.noreply.github.com>
2025-02-17 19:50:32 -0500

    Completely rewrote clean.py to clean intermediate files for command-line .TEX files.

--- clean.py
+++ clean.py
@@ -21,9 +21,12 @@ patterns, and (with -v) echo all files matched by patterns.
 
 import os
 import fnmatch
+import ssl
+import urllib.request
 import argparse
+import sys
 
-def find_files(base_dir, patterns, verbose=False):
+def find_files_dir(base_dir, patterns, verbose=False):
     """
     Search for files in a given directory matching the specified patterns.
 
@@ -35,11 +38,43 @@ def find_files(base_dir, patterns, verbose=False):
     Returns:
         list: A sorted list of unique files that match the given patterns.
     """
+    # TODO: removed -d option, so find_files_dir is not used
     matches = dict()
     for root, dirs, files in os.walk(base_dir):
         for pattern in patterns:
             for filename in fnmatch.filter(files, pattern):
                 file_path = os.path.join(root, filename)
+                matches[pattern] = matches.get(pattern, list()) + [file_path]
+    if verbose:
+        for k, v in matches.items():
+            print(f"# {k} \u2192 {v}:")
+    return sorted(set(f for k, v in matches.items() for f in v))
+
+def find_files_tex(tex_files, patterns, verbose=False):
+    """
+    Search for files matching the specified patterns with the same head as the files
+    in the tex_files .TEX files list.
+
+    Args:
+        tex_files (list): List of .TEX files.
+        patterns (list): List of patterns (glob-style) to match files against.
+        verbose (bool): If True, prints each pattern and its matched files.
+
+    Returns:
+        list: A sorted list of unique files matching the given patterns for tex_files.
+    """
+    matches = dict()
+    for pathname in tex_files:
+        path, name = os.path.split(os.path.realpath(pathname))
+        head, tail = os.path.splitext(name)
+        assert tail.lower() == '.tex', f"{pathname} not .TEX"
+        # print('*', path, name, head, tail, f"{head}.*", end=' ')
+        files = fnmatch.filter(os.listdir(path), f"{head}.*")
+        # print(files)    
+        for pattern in patterns:
+            #if fnmatch.filter(files, pattern): print('matches:', fnmatch.filter(files, pattern))
+            for filename in fnmatch.filter(files, pattern):
+                file_path = os.path.join(path, filename)
                 matches[pattern] = matches.get(pattern, list()) + [file_path]
     if verbose:
         for k, v in matches.items():
@@ -60,7 +95,29 @@ def remove_file(filename, dry_run=False):
         print(f"rm {filename}")
         os.remove(filename) # FILE REMOVAL
 
-def main(patterns):
+def get_patterns(uri='https://www.toptal.com/developers/gitignore/api/latex'):
+    """
+    Read LaTeX toptal.com .gitignore glob extensions.
+
+    Args:
+        uri (str): URI
+
+    Returns:
+        list: A list of LaTeX toptal.com .gitignore glob extensions
+    """
+    headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" }
+    try:
+        request = urllib.request.Request(uri, headers=headers)  # create request w/ User-Agent
+        context = ssl._create_unverified_context()              # create unverified SSL context
+        with urllib.request.urlopen(request, context=context) as response:
+            lines = [ line for line in response.read().decode("utf-8").splitlines()
+                if line and not line.startswith('#') ]
+    except Exception as e:
+        print(f"An error occurred: '{e}'")
+        lines = list()
+    return lines
+
+def main(args, patterns):
     """
     Parse command-line arguments, search for files, and handle their removal.
 
@@ -68,27 +125,30 @@ def main(patterns):
         patterns (list): List of file patterns to search for.
     """
     parser = argparse.ArgumentParser(description='Delete extraneous TeX files from subdirectories.')
-    parser.add_argument('-d', '--dir', default='.', help='Top directory to search (default: current directory).')
+    # TODO: removed -d option in favor of .TEX files
+    # parser.add_argument('-d', '--dir', default='.', help='Top directory to search (default: current directory).')
+    parser.add_argument('-e', '--ext', action='append', help='Extension(s) to add to .gitignore extension list.')
     parser.add_argument('-n', '--dry-run', action='store_true', help='Log file removal without actual file deletion.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print verbose output.')
-    parser.add_argument('extension', nargs='*', help='File extensions to match (e.g., pdf).')
+    parser.add_argument('files', nargs='*', help='.TEX files to clean.')
 
-    args = parser.parse_args()
-    base_dir = args.dir
+    args = parser.parse_args(args)
+    # base_dir = args.dir
+    extensions = args.ext if args.ext else list()
     dry_run = args.dry_run
     verbose = args.verbose
-    extensions = args.extension
+    files = args.files
     
-    if not os.path.isdir(base_dir):
-        print(f"Directory '{base_dir}' does not exist.")
-        return
+    # if not os.path.isdir(base_dir):
+    #     print(f"Directory '{base_dir}' does not exist.")
+    #     return
 
     # Add user-specified patterns
     for ext in extensions:
         patterns.append(f"*.{ext}")
 
     # Find files based on patterns
-    filenames = find_files(base_dir, patterns, verbose)
+    filenames = find_files_tex(files, patterns, verbose)
 
     for filename in filenames:
         base_name = os.path.splitext(filename)[0]
@@ -99,163 +159,16 @@ def main(patterns):
         remove_file(filename, dry_run)
 
 if __name__ == "__main__":
-    # Using splitlines to handle the patterns, each pattern on a new line
-    patterns = [g for g in """
-_minted*
-.*.lb
-.*.swp
-./auto/*
-.pdf
-.texpadtmp
-*-blx.aux
-*-blx.bib
-*-concordance.tex
-*-converted-to.*
-*-gnuplottex-*
-*-tags.tex
-*-tikzDictionary
-*.?end
-*.[1-9]
-*.[1-9][0-9]
-*.[1-9][0-9][0-9]
-*.[1-9][0-9][0-9]R
-*.[1-9][0-9]R
-*.[1-9]R
-*.4ct
-*.4tc
-*.acn
-*.acr
-*.alg
-*.aux
-*.auxlock
-*.backup
-*.bak
-*.bbl
-*.bcf
-*.blg
-*.brf
-*.cb
-*.cb2
-*.cpt
-*.cut
-*.dpth
-*.dvi
-*.el
-*.eledsec[1-9]
-*.eledsec[1-9][0-9]
-*.eledsec[1-9][0-9][0-9]
-*.eledsec[1-9][0-9][0-9]R
-*.eledsec[1-9][0-9]R
-*.eledsec[1-9]R
-*.end
-*.ent
-*.fdb_latexmk
-*.fff
-*.fls
-*.fmt
-*.fot
-*.gaux
-*.glg
-*.glo
-*.glog
-*.gls
-*.glsdefs
-*.glstex
-*.gnuplot
-*.gtex
-*.hst
-*.idv
-*.idx
-*.ilg
-*.ind
-*.lg
-*.listing
-*.loa
-*.lod
-*.loe
-*.lof
-*.log
-*.lol
-*.lot
-*.lox
-*.lpz
-*.ltjruby
-*.lyx~
-*.lzo
-*.lzs
-*.maf
-*.md5
-*.mf
-*.mlf
-*.mlt
-*.mp
-*.mtc[0-9]*
-*.mw
-*.nav
-*.newpax
-*.nlg
-*.nlo
-*.nls
-*.out
-*.pax
-*.pdfpc
-*.pdfsync
-*.pre
-*.ptc
-*.pyg
-*.pytxcode
-*.run.xml
-*.sagetex.py
-*.sagetex.sage
-*.sagetex.scmd
-*.sav
-*.slf[0-9]*
-*.slg
-*.slo
-*.sls
-*.slt[0-9]*
-*.snm
-*.soc
-*.sout
-*.spl
-*.sta
-*.stc[0-9]*
-*.sympy
-*.synctex
-*.synctex.gz
-*.synctex.gz(busy)
-*.synctex(busy)
-*.t[1-9]
-*.t[1-9][0-9]
-*.table
-*.tdo
-*.tfm
-*.thm
-*.toc
-*.tps
-*.trc
-*.ttt
-*.upa
-*.upb
-*.ver
-*.vrb
-*.vtc
-*.wrt
-*.xcp
-*.xdv
-*.xdy
-*.xmpi
-*.xref
-*.xwm
-*.xyc
-*.xyd
-*~[0-9]*
-acs-*.bib
-latex.out/
-pythontex-files-*/
-svg-inkscape/
-sympy-plots-for-*.tex/
-TSWLatexianTemp*
-""".splitlines() if g ]
-    
-    main(patterns)
+    is_idle, is_pycharm, is_jupyter = (
+        'idlelib' in sys.modules,
+        int(os.getenv('PYCHARM', 0)),
+        '__file__' not in globals()
+        )
+    if is_idle or is_pycharm or is_jupyter:
+        main([ '-n', '-v', '-e', 'pdf',
+               'doc/math/math-tex-example.tex',
+               'doc/math/license-plates-example.tex',
+               'foo.tex', ], get_patterns())
+    else:
+        # https://stackoverflow.com/a/76997871/17467335 re: sys.argv[1: ]
+        main(sys.argv[1: ], get_patterns())
